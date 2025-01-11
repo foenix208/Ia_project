0b5f882e1dd9addd43b8c2a5e5b7c0ed1a62d94b
Mael Eouzan <mael.eouzan@gmail.com>
2025-01-11 15:44:45 +0100

    removed error proning and time consuming shit what does proning even mean ? idk

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from src.algo3 import algo3
 
 import numpy as np
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-
 def load_data(filepath):
     X, y = [], []
     with open(filepath, 'r') as file:
@@ -24,11 +21,6 @@ def main():
     # Charger les données d'entraînement et de test
     X_train, y_train = load_data('data/poker-hand-training-true.data')
     X_test, y_test = load_data('data/poker-hand-testing.data')
-
-    clf = SVC(kernel="linear")
-    clf = clf.fit(X_train, y_train)
-
-    Z = clf.predict (X_test)
 
     algo1()
     # algo2(X_train, X_test, y_train, y_test)
